4423632aa2925d7e458c10995dd65c72686699ff
EGURGINE <93648712+EGURGINE@users.noreply.github.com>
2022-05-31 07:54:27 +0900

    메인과 연결함

--- Assets/Scripts/StageManager.cs
+++ Assets/Scripts/StageManager.cs
@@ -5,6 +5,17 @@ using UnityEngine.SceneManagement;
 
 public class StageManager : MonoBehaviour
 {
+    [SerializeField] private GameObject pc;
+    public void MainBtn()
+    {
+        SceneManager.LoadScene("Main");
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButton(0)) Instantiate(pc).transform.position =
+                  Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 10));
+    }
     public void Game_1()
     {
         SceneManager.LoadScene("Game1");
