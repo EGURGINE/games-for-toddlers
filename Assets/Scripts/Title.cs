de6f245e869177167d80c5ef18c544849600adfe
EGURGINE <93648712+EGURGINE@users.noreply.github.com>
2022-06-27 16:00:02 +0900

    타이틀

--- Assets/Scripts/Title.cs
+++ Assets/Scripts/Title.cs
@@ -5,6 +5,7 @@ using UnityEngine.SceneManagement;
 public class Title : MonoBehaviour
 {
     [SerializeField] private Image title;
+    [SerializeField] private GameObject credit;
     void Start()
     {
         title.rectTransform.DOLocalMoveY(200, 1).SetLoops(-1, LoopType.Yoyo);
@@ -13,9 +14,17 @@ public class Title : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButton(0))
-        {
-            SceneManager.LoadScene("Main");
-        }
+        //if (Input.GetMouseButton(0))
+        //{
+        //    SceneManager.LoadScene("Main");
+        //}
+    }
+    public void creditON()
+    {
+        credit.transform.DOLocalMoveY(0,1).SetEase(Ease.InOutSine);
+    }
+    public void creditOff()
+    {
+        credit.transform.DOLocalMoveY(-1099, 1).SetEase(Ease.InOutSine);
     }
 }
